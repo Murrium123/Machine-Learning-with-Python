2be94f916f642875c0c27e3d5ae6af07c857fc20
Em <murriumcra09@gmail.com>
2026-08-21 17:18:55 -0400

    Import necessary libraries for classification

--- 7.2.1classifacationMatrix.py
+++ 7.2.1classifacationMatrix.py
@@ -1,1 +1,4 @@
+from sklearn.metrics import (confusion_matrix, ConfusionMatrixDisplay, accuracy_score, precision_score, recall_score, f1_score, roc_auc_score)
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_breast_cancer
 
