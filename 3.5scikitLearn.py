0eacab7cd6d9e9c870c1d084a92a8f573e2e03f9
Em <murriumcra09@gmail.com>
2026-08-29 14:09:55 -0400

    Fix random data generation for X in scikitLearn.py

--- 3.5scikitLearn.py
+++ 3.5scikitLearn.py
@@ -2,3 +2,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import numpy as np
+
+np.random.seed(42)
+X = np.random.randn(200,1)
